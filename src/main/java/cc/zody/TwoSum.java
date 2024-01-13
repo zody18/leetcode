4525d390276f2838b59df81131b76e515bf8c107
zody <minliu.engineer@gmail.com>
2024-01-13 12:49:24 +0800

    second commit

--- src/main/java/cc/zody/TwoSum.java
+++ src/main/java/cc/zody/TwoSum.java
@@ -1,2 +1,22 @@
-package cc.zody;public class TwoSum {
+package cc.zody;
+
+
+/**
+ * <a href="https://leetcode.cn/problems/two-sum/description/">题目地址</a>
+ *
+ * @author zody
+ * @since 2024-01-13
+ */
+public class TwoSum {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public int[] twoSum(int[] numbers, int target) {
+        return null;
+    }
+
+
 }
